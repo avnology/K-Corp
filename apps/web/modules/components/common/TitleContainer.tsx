6606d2371815e3193518db5ac5a38e0d98d49fcc
adhamalfy <104334929+adhamalfy@users.noreply.github.com>
2025-12-10 17:40:20 +0200

    fix(TitleContainer): improve whitespace handling and clean up class names

--- apps/web/modules/components/common/TitleContainer.tsx
+++ apps/web/modules/components/common/TitleContainer.tsx
@@ -13,25 +13,31 @@ export default function TitleContainer({ title }: { title: string }) {
 
   return (
     <div className="w-full bg-primary border-y-2 border-white text-white overflow-hidden relative">
-      <div className="w-full border-b border-white py-2 flex items-center justify-between px-6 text-[5px] md:text-[10px] opacity-70 tracking-widest">
+      <div className="w-full border-b border-white py-2 flex items-center justify-between px-6 text-[5px] md:text-[10px] opacity-70 tracking-widest whitespace-nowrap overflow-hidden">
         <span>►</span>
         {Array.from({ length: 8 }).map((_, i) => (
-          <span className="flex items-center gap-4" key={i}>
+          <span
+            className={`items-center gap-4 ${i > 2 ? "hidden sm:flex" : "flex"}`}
+            key={i}
+          >
             {sequence()}
           </span>
         ))}
       </div>
 
       <div className="w-full flex justify-center text-center items-center py-16">
-        <p className="xl:text-[140px] md:text-[110px] text-[60px] sm:text-[70px] 2xl:text-[165px] l font-extrabold uppercase">
+        <p className="xl:text-[140px] md:text-[110px] text-[60px] sm:text-[70px] 2xl:text-[165px] font-extrabold uppercase">
           {title}
         </p>
       </div>
 
-      <div className="w-full border-t border-white py-2 flex items-center justify-between px-6 text-[5px] md:text-[10px] opacity-70 tracking-widest">
+      <div className="w-full border-t border-white py-2 flex items-center justify-between px-6 text-[5px] md:text-[10px] opacity-70 tracking-widest whitespace-nowrap overflow-hidden">
         <span>►</span>
         {Array.from({ length: 5 }).map((_, i) => (
-          <span className="flex items-center gap-4" key={i}>
+          <span
+            className={`items-center gap-4 ${i > 2 ? "hidden sm:flex" : "flex"}`}
+            key={i}
+          >
             {sequence()}
           </span>
         ))}
